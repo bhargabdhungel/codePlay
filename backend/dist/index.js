906a480b6d12019ec5dcb6d11078e084c241af79
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-03-24 18:15:24 +0530

    init2

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+const port = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server is healthy and running on port ${port}`);
 });
