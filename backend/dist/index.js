c990698c37c55633d36ae24f7106e416df953b6f
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-07 03:39:50 +0530

    bs1

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,18 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const index_1 = __importDefault(require("./routes/auth/index"));
+// import auth from "./routes/auth/index";
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT;
-app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use('/auth', index_1.default);
+app.use(express_1.default.json());
+// app.use('/auth', auth);
 app.get('/', (req, res) => {
     console.log('Hello World!');
     res.send('Hello World!');
 });
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is healthy and running on port ${port}`);
 });
