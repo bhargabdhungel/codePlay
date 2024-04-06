c6966c17704ea6f539b3be83c7b500f06216b6fb
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-07 03:38:06 +0530

    bs1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import auth from "./routes/auth/index";
+// import auth from "./routes/auth/index";
 import cors from 'cors';
 
 dotenv.config();
 
 
 const app = express();
-const port = process.env.PORT;
-
+app.use(cors());
 app.use(express.json());
 
-app.use(cors());
-app.use('/auth', auth);
+
+// app.use('/auth', auth);
 
 
 app.get('/', (req, res) => {
@@ -20,6 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is healthy and running on port ${port}`);
 });
