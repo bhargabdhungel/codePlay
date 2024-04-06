53c5cc96f0f310b096397aa67b4aaba831fd0b13
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-07 03:44:59 +0530

    bs2

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,13 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// import auth from "./routes/auth/index";
+const index_1 = __importDefault(require("./routes/auth/index"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-// app.use('/auth', auth);
+app.use('/auth', index_1.default);
 app.get('/', (req, res) => {
     console.log('Hello World!');
     res.send('Hello World!');
