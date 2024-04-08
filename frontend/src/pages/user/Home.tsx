667db31a148c407c5b6341e5caf47a9acd9fb038
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-04-08 19:30:48 +0530

    infinite loop fix

--- frontend/src/pages/user/Home.tsx
+++ frontend/src/pages/user/Home.tsx
@@ -5,6 +5,7 @@ import Search from "../../components/Search";
 import { SearchOptions } from "../../assets/searchData";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import { useEffect } from "react";
 
 export default function Home() {
   useSearchFeature();
@@ -13,14 +14,16 @@ export default function Home() {
   const searchValue = useRecoilValue(searchValueAtom);
   const navigate = useNavigate();
 
-  if (loading) return <div>Loading...</div>;
-  if (showSearchBar) return <Search />;
-  if (searchValue) {
-    if (searchValue == SearchOptions.Home) navigate("/home");
-    else if (searchValue == SearchOptions.Logout) {
+  useEffect(() => {
+    if (!searchValue) navigate("/home");
+    if (searchValue == SearchOptions.Logout) {
       localStorage.removeItem("token");
       navigate("/login");
     }
-  }
+    if (searchValue == SearchOptions.Home) navigate("/home");
+  }, [searchValue, navigate]);
+
+  if (loading) return <div>Loading...</div>;
+  if (showSearchBar) return <Search />;
   return <div>you searched for: {searchValue}</div>;
 }
